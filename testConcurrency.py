9799c6efa3e1bfffc27248e324aaef29f41a44bf
Harshith Suresh <harshiths2004@gmail.com>
2024-11-29 16:17:08 -0500

    added indexes and concurrency

---
+++ testConcurrency.py
@@ -0,0 +1,20 @@
+import requests
+from concurrent.futures import ThreadPoolExecutor
+
+url = "http://127.0.0.1:5000/borrow_book/{user_id}/{book_id}"
+
+def borrow_book_request(user_id, book_id):
+    try:
+        response = requests.post(url.format(user_id=user_id, book_id=book_id))
+        try:
+            json_response = response.json()
+            print(f"User {user_id} - Book {book_id}: {response.status_code} - {json_response}")
+        except ValueError:
+            print(f"User {user_id} - Book {book_id}: Invalid JSON response - {response.text}")
+    except Exception as e:
+        print(f"Error for User {user_id} - Book {book_id}: {e}")
+
+if __name__ == "__main__":
+    with ThreadPoolExecutor() as executor:
+        executor.submit(borrow_book_request, 2, 3)
+        executor.submit(borrow_book_request, 3, 3) 
